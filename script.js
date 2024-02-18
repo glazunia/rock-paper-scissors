8d9538c5067eb088a22faea87754333fb90b12ed
glazunia <mashka.bor@yandex.ru>
2024-02-18 18:40:23 +0300

    Create playRound function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const playerSelection = 'Rock'; // change to prompt later
 const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
@@ -9,5 +10,24 @@ function getComputerChoice() {
             return 'Paper';
         case 2:
             return 'Scissors';
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `Tie! Both computer and you chose ${playerSelection}!`;
+    } else if (
+        (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+        (playerSelection === 'Paper' && computerSelection === 'Rock')
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    } 
+    // to come around 'scissors beats'
+      else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return `You win! ${playerSelection} beat ${computerSelection}!`;
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return `Computer wins! ${computerSelection} beat ${playerSelection}!`;
+    } else {
+        return `Computer wins! ${computerSelection} beats ${playerSelection}!`;
     }
 }
